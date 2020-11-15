d42c04119f1d9da1b8881b3e9380fd3189bda787
Divyansh Barar <68277579+divyanshbarar@users.noreply.github.com>
2020-11-15 23:51:47 +0530

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -5,3 +5,7 @@ class Task(models.Model):
     title=models.CharField(max_length=200)
     complete=models.BooleanField(default=False)
     created=models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
