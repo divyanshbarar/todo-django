ceb8d2b4d5adba18b3265668f2563afaa6e21ae8
Divyansh Barar <68277579+divyanshbarar@users.noreply.github.com>
2020-11-21 23:57:01 +0530

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+
+
+admin.site.site_header="TODO Page Admin"
+admin.site.site_title=" Welcome Divyansh"
+admin.site.index_title="This is TODO Page Backend"
+
 urlpatterns = [
     path('',views.index,name="home"),
     path('update/<str:pk>/',views.updateTask,name="update"),
